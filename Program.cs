2866932922e95c0841994e40eb5f39c684279f64
julian-code <email@codeiwrote.net>
2020-10-27 16:14:49 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Couchbase;
+
+namespace Fremlæggelse
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var cluster = await Cluster.ConnectAsync("couchbase://localhost", "admin", "123456");
+
+            var rows = await cluster.QueryAsync<dynamic>("select * from `beer-sample`");
+
+            await foreach (var item in rows)
+            {
+                System.Console.WriteLine(item);
+            }
+        }
+    }
+}
